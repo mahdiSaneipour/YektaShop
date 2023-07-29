92709900c01d0f0b86865c432660d60a4eccfdeb
mahdiSaneipour <freelancerdtms@gmail.com>
2023-07-29 17:46:47 +0330

    Mahdi - Add Profile Layout - 5:46 pm 7/29/2023

--- BN_Project.Domain/Entities/Category.cs
+++ BN_Project.Domain/Entities/Category.cs
@@ -6,7 +6,7 @@ namespace BN_Project.Domain.Entities
     public class Category : BaseEntity
     {
         public string Title { get; set; }
-        public int ParentId { get; set; }
+        public int? ParentId { get; set; }
         [ForeignKey("ParentId")]
         public Category Catagory { get; set; }
     }
